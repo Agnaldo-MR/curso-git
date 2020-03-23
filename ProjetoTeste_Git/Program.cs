59a4ede67004589708c13ade375a7f10350cc859
Agnaldo MR <agnaldo.m.r@hotmail.com>
2020-03-23 13:58:13 -0300

    Acrescentada um mensagem de Bom Dia

--- ProjetoTeste_Git/Program.cs
+++ ProjetoTeste_Git/Program.cs
@@ -7,6 +7,8 @@ namespace ProjetoTeste_Git
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Bom dia!");
+
             Console.ReadLine();
         }
     }
