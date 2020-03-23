b46dc78149d221742b12d549422dd5a66d403fc9
Agnaldo MR <agnaldo.m.r@hotmail.com>
2020-03-23 13:52:00 -0300

    Projeto iniciado, mostrando a mensagem Ola Mundo

---
+++ ProjetoTeste_Git/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ProjetoTeste_Git
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadLine();
+        }
+    }
+}
