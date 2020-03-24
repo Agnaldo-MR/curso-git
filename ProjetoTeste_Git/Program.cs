c1a44efa6f4ad6278d95f33a25a6dd974aa56113
Agnaldo MR <agnaldo.m.r@hotmail.com>
2020-03-24 10:45:18 -0300

    Acrescentada a pergunta sobre como voce esta hoje

--- ProjetoTeste_Git/Program.cs
+++ ProjetoTeste_Git/Program.cs
@@ -8,6 +8,7 @@ namespace ProjetoTeste_Git
         {
             Console.WriteLine("Hello World!");
             Console.WriteLine("Bom dia!");
+            Console.WriteLine("Como você está hoje?");
 
             Console.ReadLine();
         }
